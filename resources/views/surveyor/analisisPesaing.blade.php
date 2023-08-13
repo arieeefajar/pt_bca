9d2623baeabd9c80217b126c7ce3ea66f8e8ba15
RissAgung <risqiagungg246@gmail.com>
2023-08-13 18:25:43 +0700

    fix: Backend and read data in view analisis pesaing

--- resources/views/surveyor/analisisPesaing.blade.php
+++ resources/views/surveyor/analisisPesaing.blade.php
@@ -19,40 +19,102 @@
                     <div class="live-preview">
                         <div class="table-responsive">
                             <form action="" method="POST">
-                                <div class="bg-soft-primary p-2 mb-2">
-                                    <h6 class="text-center">Gambaran Umum Situasi Perusahaan</h6>
-                                </div>
 
-                                <div class="content mb-3">
-                                    <label for="nama">Nama</label>
-                                    <ul id="tagList">
-                                        <input type="text" class="form-control" id="inputTags">
-                                    </ul>
-                                </div>
+                                {{-- struktur array: section-->jenis jawaban-->data pertanyaan --}}
 
-                                <table class="table table-bordered align-middle table-nowrap mb-0">
-                                    <thead>
-                                        <tr class="bg-soft-primary">
-                                            <th class="text-center" colspan="6">Persaingan diantara Perusahaan</th>
-                                        </tr>
-                                        <tr class="bg-soft-warning">
-                                            <th class="text-center">Pertanyaan</th>
-                                            <th class="text-center">Ya</th>
-                                            <th class="text-center">Tidak</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Contoh Pertanyaan</td>
-                                            <td align="center">
-                                                <input type="radio" name="pertanyaan1" value="1">
-                                            </td>
-                                            <td align="center">
-                                                <input type="radio" name="pertanyaan1" value="2">
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                {{-- extrack section --}}
+                                @foreach ($dataPertanyaan as $section => $jenisPertanyaan)
+                                    <div class="bg-soft-primary p-2">
+                                        <h6 class="text-center">{{ $section }}</h6>
+                                    </div>
+
+                                    {{-- extrack jenis jawaban --}}
+                                    @foreach ($jenisPertanyaan as $idJenisPertanyaan => $datapertanyaan)
+                                        {{-- pecah pertanyaan berdasarkan jenis jawabannya --}}
+                                        @if ($idJenisPertanyaan == '1')
+                                            {{-- skala evaluasi (1-5) --}}
+                                            <div class="table-responsive mb-3">
+                                                <table class="table table-bordered align-middle table-nowrap mb-0">
+                                                    <thead>
+                                                        <tr class="bg-soft-warning">
+                                                            <th class="text-center">Pertanyaan</th>
+                                                            <th class="text-center">1</th>
+                                                            <th class="text-center">2</th>
+                                                            <th class="text-center">3</th>
+                                                            <th class="text-center">4</th>
+                                                            <th class="text-center">5</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {{-- extrack data pertanyaan --}}
+                                                        @foreach ($datapertanyaan as $data)
+                                                            <tr>
+                                                                <td>{{ $data['pertanyaan'] }}</td>
+                                                                <td align="center">
+                                                                    <input type="radio" name="pertanyaan1" value="1">
+                                                                </td>
+                                                                <td align="center">
+                                                                    <input type="radio" name="pertanyaan1" value="2">
+                                                                </td>
+                                                                <td align="center">
+                                                                    <input type="radio" name="pertanyaan1" value="3">
+                                                                </td>
+                                                                <td align="center">
+                                                                    <input type="radio" name="pertanyaan1" value="4">
+                                                                </td>
+                                                                <td align="center">
+                                                                    <input type="radio" name="pertanyaan1" value="5">
+                                                                </td>
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        @elseif ($idJenisPertanyaan == '2')
+                                            {{-- skala pendapat (ya/tidak) --}}
+                                            <div class="table-responsive mb-3">
+                                                <table class="table table-bordered align-middle table-nowrap mb-0">
+                                                    <thead>
+                                                        <tr class="bg-soft-warning">
+                                                            <th class="text-left">Pertanyaan</th>
+                                                            <th class="text-center">Ya</th>
+                                                            <th class="text-center">Tidak</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {{-- extrack data pertanyaan --}}
+                                                        @foreach ($datapertanyaan as $data)
+                                                            <tr>
+                                                                <td>{{ $data['pertanyaan'] }}</td>
+                                                                <td align="center">
+                                                                    <input type="radio" name="pertanyaan1" value="1">
+                                                                </td>
+                                                                <td align="center">
+                                                                    <input type="radio" name="pertanyaan1" value="2">
+                                                                </td>
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        @elseif($idJenisPertanyaan == '3')
+                                            {{-- tanggapan terbuka (field) --}}
+                                            <div class="mb-3">
+                                                {{-- extrack data pertanyaan --}}
+                                                @foreach ($datapertanyaan as $data)
+                                                    <div class="content mt-2">
+                                                        <label for="nama">{{ $data['pertanyaan'] }}</label>
+                                                        <ul id="tagList">
+                                                            <input type="text" class="form-control" id="inputTags">
+                                                        </ul>
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        @endif
+                                    @endforeach
+                                @endforeach
+
+
                                 <div class="text-center mt-3">
                                     <button type="button" class="btn btn-primary">Submit</button>
                                 </div>
