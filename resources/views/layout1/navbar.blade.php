8f47bd0e7162eea6fc4cc884a5d27982c3e543ee
arieeefajar <ariefajar739@gmail.com>
2023-09-08 16:22:50 +0700

    remove search in navbar

--- resources/views/layout1/navbar.blade.php
+++ resources/views/layout1/navbar.blade.php
@@ -36,9 +36,8 @@
             <!-- App Search-->
             <form class="app-search d-none d-md-block">
                 <div class="position-relative">
-                    <input type="text" class="form-control" placeholder="Search..." autocomplete="off"
+                    <input type="hidden" class="form-control" placeholder="Search..." autocomplete="off"
                         id="search-options" value="">
-                    <span class="mdi mdi-magnify search-widget-icon"></span>
                     <span class="mdi mdi-close-circle search-widget-icon search-widget-icon-close d-none"
                         id="search-close-options"></span>
                 </div>
