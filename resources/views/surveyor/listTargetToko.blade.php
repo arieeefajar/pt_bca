03e128769cd665c6b07d0ae7148b4fea50903ebc
arieeefajar <ariefajar739@gmail.com>
2023-09-14 14:45:48 +0700

    add tampilan forgotPassword

--- resources/views/surveyor/listTargetToko.blade.php
+++ resources/views/surveyor/listTargetToko.blade.php
@@ -15,19 +15,9 @@
 
                     <div class="card-body">
                         <div id="customerList">
-                            <div class="row g-4 mb-3">
-                                <div class="col-sm-auto">
-                                    <div class="d-flex justify-content-sm-end">
-                                        <div class="search-box ms-2">
-                                            <input type="text" class="form-control search" placeholder="Search...">
-                                            <i class="ri-search-line search-icon"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
 
-                            <div class="table-responsive table-card mt-3 mb-1">
-                                <table class="table align-middle table-nowrap" id="customerTable">
+                            <div class="table-responsive mt-3 mb-1">
+                                <table class="table align-middle table-nowrap" id="myTable">
                                     <thead class="table-light text-center">
                                         <tr>
                                             <th scope="col" data-sort="no">No</th>
@@ -42,8 +32,6 @@
                                         @foreach ($dataPerusahaan as $index => $data)
                                             <tr>
                                                 <th scope="row">{{ $index + 1 }}</th>
-                                                <td class="id" style="display:none;"><a href="javascript:void(0);"
-                                                        class="fw-medium link-primary">#VZ2101</a></td>
                                                 <td class="customer_name">{{ $data->nama }}</td>
                                                 <td class="email">{{ $data->jenis }}</td>
                                                 <td class="phone">{{ $data->provinsi }}</td>
@@ -53,28 +41,6 @@
                                         @endforeach
                                     </tbody>
                                 </table>
-                                <div class="noresult" style="display: none">
-                                    <div class="text-center">
-                                        <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop"
-                                            colors="primary:#121331,secondary:#08a88a" style="width:75px;height:75px">
-                                        </lord-icon>
-                                        <h5 class="mt-2">Sorry! No Result Found</h5>
-                                        <p class="text-muted mb-0">We've searched more than 150+ Orders We did not find any
-                                            orders for you search.</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="d-flex justify-content-end">
-                                <div class="pagination-wrap hstack gap-2">
-                                    <a class="page-item pagination-prev disabled" href="#">
-                                        Previous
-                                    </a>
-                                    <ul class="pagination listjs-pagination mb-0"></ul>
-                                    <a class="page-item pagination-next" href="#">
-                                        Next
-                                    </a>
-                                </div>
                             </div>
                         </div>
                     </div><!-- end card -->
@@ -85,4 +51,14 @@
         </div>
         <!-- end row -->
     </div>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable({
+                columnDefs: [{
+                    targets: '_all', // Menonaktifkan urutan untuk semua kolom
+                    sortable: false
+                }]
+            });
+        });
+    </script>
 @endsection
